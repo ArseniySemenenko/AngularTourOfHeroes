3122dfa55a57800925502b6569f9000adbba3493
ArseniySemenenko <arseniysemenenko65@gmail.com>
2026-06-08 20:36:47 +0300

    Create hero-detail component

---
+++ src/app/components/hero-detail/hero-detail.ts
@@ -0,0 +1,14 @@
+import { Component , Input} from '@angular/core';
+import { IHero } from '../../models';
+import { UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-hero-detail',
+  imports: [UpperCasePipe , FormsModule],
+  templateUrl: './hero-detail.html',
+  styleUrl: './hero-detail.css',
+})
+export class HeroDetail {
+  @Input() hero?: IHero;
+}
--- src/app/components/heroes-list/heroes-list.ts
+++ src/app/components/heroes-list/heroes-list.ts
@@ -3,13 +3,18 @@ import { IHero } from '../../models';
 import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {HEROES} from "../../mock";
+import { HeroDetail } from "../hero-detail/hero-detail";
 
 @Component({
   selector: 'app-heroes-list',
-  imports: [UpperCasePipe , FormsModule],
+  imports: [UpperCasePipe, FormsModule, HeroDetail],
   templateUrl: './heroes-list.html',
   styleUrl: './heroes-list.css',
 })
 export class HeroesList {
   heroes=HEROES;
+  selectedHero?: IHero;
+  onSelect(hero: IHero): void{
+    this.selectedHero = hero;
+  }
 }
